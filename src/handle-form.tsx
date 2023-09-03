0eb8aed7d89bee411fc8fae601038e7169e64563
Scott Clingan <sclingan1@gmail.com>
2023-09-03 00:01:10 -0500

    Adding form validation

--- src/handle-form.tsx
+++ src/handle-form.tsx
@@ -7,4 +7,13 @@ export function showInput(event: any) {
         const email: string  = input.value;
         console.log(email);
         console.log(typeof(email));
+        // validate email
+        // find a regex that works!
+        const regex = /(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@[*[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+]*/;
+        const answer : boolean = regex.test(email);
+        if(answer) {
+            console.log('passed');
+        } else {
+            console.log('failed');
+        }
 }
