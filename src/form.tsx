09a4d89b2d67d163b66fe84293c88604311973ee
Scott Clingan <sclingan1@gmail.com>
2023-09-03 23:33:06 -0500

    Adding useState for form validation

--- src/form.tsx
+++ src/form.tsx
@@ -1,10 +1,24 @@
-import { showInput } from "./handle-form";
+import { useState } from 'react';
+
 
 function Form() {
+
+    const [email, setEmail] = useState('');
+
+    function showInput(e: any) {
+        e.preventDefault();
+        console.log(email);
+
+        // Find a better regex to test
+        const regex = /(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@[*[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+]*/;
+        const answer : boolean = regex.test(email);
+        console.log(answer);
+    }
+
     return(
         <form>
              <label htmlFor='email'>Email address<span className="sr-only">Valid email required</span></label>
-             <input type='email' id='email' placeholder='email@company.com' autoComplete="off"></input>
+             <input type='email' id='email' value={email} onChange={e => setEmail(e.target.value)}placeholder='email@company.com' autoComplete="off"></input>
              <button onClick={showInput}>Subscribe to monthly newsletter</button>
         </form>
     )
