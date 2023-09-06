32177f75cd315eb8832098a93dfd5ad9d115661e
Scott Clingan <sclingan1@gmail.com>
2023-09-06 14:57:07 -0500

    Adding error states, fixing desktop layout

--- src/form.tsx
+++ src/form.tsx
@@ -12,15 +12,23 @@ function Form() {
         // Find a better regex to test
         const regex = /(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@[*[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+]*/;
         const answer : boolean = regex.test(email);
-        console.log(answer);
         // on success open success.tsx
         // on error, set error states, show active error states
+        if(answer) {
+            console.log('passed');
+        }else{
+            console.log('failed');
+            let error = document.getElementsByTagName('span');
+            error[0].classList.remove('sr-only');
+            error[0].classList.add('error');
+            console.log(error);
+        }
     }
 
     return(
         <form>
              <label htmlFor='email'>Email address<span className="sr-only">Valid email required</span></label>
-             <input type='email' id='email' value={email} onChange={e => setEmail(e.target.value)}placeholder='email@company.com' autoComplete="off"></input>
+             <input type='email' id='email' value={email} onChange={e => setEmail(e.target.value)}placeholder='email@company.com' autoComplete="off" className='input-error'></input>
              <button onClick={showInput}>Subscribe to monthly newsletter</button>
         </form>
     )
